bfd8a13b1735b0873e93b30b443672d5761182fb
Monsavon87 <aelikania@gmail.com>
2024-05-21 14:39:04 +0200

    init
    -init from game template from bacondandgames

---
+++ Resources/user_prefs.gd
@@ -0,0 +1,22 @@
+class_name UserPrefs extends Resource
+
+@export_range(0,1,.05) var music_volume: float = 1.0
+@export_range(0,1,.05) var sfx_volume: float = 1.0
+@export var use_smooth_movement:bool = true
+@export var language:int = 0
+@export var window_mode:int = 0
+@export var window_resolution: Vector2i = Vector2i(960, 540)
+@export var window_monitor: int = 0
+
+const USER_PREFS_PATH:String = "user://user_prefs.tres"
+
+func save() -> void:
+	ResourceSaver.save(self, USER_PREFS_PATH)
+	
+static func load_or_create() -> UserPrefs:
+	var res:UserPrefs
+	if FileAccess.file_exists(USER_PREFS_PATH):
+		res = load(USER_PREFS_PATH) as UserPrefs
+	else:
+		res = UserPrefs.new()
+	return res
