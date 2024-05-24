e3e8bfba8dd1389a61a305c07f4da08b432074c5
Monsavon87 <aelikania@gmail.com>
2024-05-24 12:47:29 +0200

    navigation and AI
    -managing navigation map
    -adding auto generation of navigation region and navigation polygon on level start
    -including navigation server to enemy
    -tweaking enemy AI using navigation server

--- Gameplay/Levels/level.gd
+++ Gameplay/Levels/level.gd
@@ -7,7 +7,14 @@ class_name Level extends Node2D
 
 @export var player = CharacterBody2D
 @export var doors:Array[Door]
+@export var ground_layer = TileMap 
+
+
+
 var data:LevelDataHandoff
+# Utilisation de la fonction pour obtenir les dimensions utilisées de la tilemap
+
+@onready var navigation: NavigationRegion2D = $Navigation
 
 func _ready() -> void:
 	player.disable()
@@ -16,8 +23,14 @@ func _ready() -> void:
 	if data == null: 
 		init_scene()
 		start_scene()
-		
-
+	
+	var used_rect = get_tilemap_used_rect(ground_layer)
+	# Dessiner la région à l'intérieur de la NavigationRegion2D
+	draw_navigation_region(used_rect)
+	# "Bake" la NavigationRegion2D
+	#bake_navigation_region()
+	
+	
 ## When a class implements this, SceneManager.on_content_finished_loading will invoke it
 ## to receive this data and pass it to the next scene
 func get_data():
@@ -86,4 +99,66 @@ func _disconnect_from_doors() -> void:
 		if door.player_entered_door.is_connected(_on_player_entered_door):
 			door.player_entered_door.disconnect(_on_player_entered_door)
 			
+
+
+
+# Fonction pour obtenir les dimensions utilisées de la tilemap
+func get_tilemap_used_rect(tilemap: TileMap) -> Rect2:
+	var used_cells = tilemap.get_used_cells(0) # Utilisation de la couche 0
+	#if used_cells.empty():
+		#return Rect2(0, 0, 0, 0)
+		   
+	var min_x = used_cells[0].x
+	var max_x = min_x
+	var min_y = used_cells[0].y
+	var max_y = min_y
+
+	for cell in used_cells:
+		if cell.x < min_x:
+			min_x = cell.x
+		elif cell.x > max_x:
+			max_x = cell.x
+		if cell.y < min_y:
+			min_y = cell.y
+		elif cell.y > max_y:
+			max_y = cell.y
+
+	var tile_size = tilemap.tile_set.tile_size
+	var used_rect = Rect2(
+		min_x * tile_size.x,
+		min_y * tile_size.y,
+		(max_x - min_x + 1) * tile_size.x,
+		(max_y - min_y + 1) * tile_size.y
+	)
+	
+	return used_rect
+
+# Dessiner la région à l'intérieur de la NavigationRegion2D
+func draw_navigation_region(used_rect: Rect2):
+	#var nav_region = $NavigationRegion2D  # Assure-toi de remplacer "NavigationRegion2D" par le chemin correct vers ta NavigationRegion2D
+
+	# Créer un nouveau polygone de navigation
+	var new_navigation_mesh = NavigationPolygon.new()
+	var bounding_outline = PackedVector2Array([
+		Vector2(used_rect.position.x, used_rect.position.y),
+		Vector2(used_rect.position.x, used_rect.position.y + used_rect.size.y),
+		Vector2(used_rect.position.x + used_rect.size.x, used_rect.position.y + used_rect.size.y),
+		Vector2(used_rect.position.x + used_rect.size.x, used_rect.position.y)
+	])
+	new_navigation_mesh.add_outline(bounding_outline)
+	# Définir le polygone de navigation pour la NavigationRegion2D
+	navigation.navigation_polygon = new_navigation_mesh
+	
+	# "Baker" le polygone de navigation
+	navigation.bake_navigation_polygon()
+	
+	
+	# Définir le polygone de navigation pour la NavigationRegion2D
+	#navigation.navigation_polygon = new_navigation_mesh
+	
+# "Bake" la NavigationRegion2D
+func bake_navigation_region():
+	navigation.bake()
+	
+	
 
--- entity/enemy/enemy_base.gd
+++ entity/enemy/enemy_base.gd
@@ -3,15 +3,17 @@ extends CharacterBody2D
 @export var speed = 50
 @export var nav_agent: NavigationAgent2D
 @export var target_distance = 30
-@export var move_component: MoveComponent
 @export var aggro_distance = 10
 @export var deaggro_distance = 50
+@export var navReg = NavigationRegion2D
 
+var target = null
 var target_node = null
 var home_pos = Vector2.ZERO
 var aggro_shape = CircleShape2D.new()
 var deaggro_shape = CircleShape2D.new()
-
+var map
+var path = []
 
 @onready var aggro_collision_shape_2d: CollisionShape2D = $Aggro/AggroRange/AggroCollisionShape2D
 @onready var de_aggro_collision_shape_2d: CollisionShape2D = $Aggro/DeAggroRange/DeAggroCollisionShape2D
@@ -21,7 +23,7 @@ var deaggro_shape = CircleShape2D.new()
 func _ready() -> void:
 	home_pos = self.global_position
 	
-	nav_agent.path_desired_distance = 4 
+	nav_agent.path_desired_distance = 10 
 	nav_agent.target_desired_distance = target_distance
 		
 	aggro_shape.radius = aggro_distance
@@ -29,33 +31,83 @@ func _ready() -> void:
 	deaggro_shape.radius = deaggro_distance
 	de_aggro_collision_shape_2d.set_shape(deaggro_shape)
 	
+	call_deferred("setup_navserver")
 	
 func _physics_process(delta: float) -> void:
-	if nav_agent.is_navigation_finished():
-		return
-	var axis = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = axis * speed
+	#if nav_agent.is_navigation_finished():
+		#return
+	##var axis = to_local(nav_agent.get_next_path_position()).normalized()
+	##var axis = nav_agent.get_next_path_position() - global_position
+	#var current_pos = global_position
+	#var next_path_pos = nav_agent.get_next_path_position()
+	#var axis = current_pos.direction_to(next_path_pos)
+	#
+	#velocity = axis * speed
+	#
+	#move_and_slide()
+	pass
 	
-	move_and_slide()
-	
+func _process(delta: float) -> void:
+	var walk_distance = speed * delta
+	move_along_path(walk_distance)
+
+func move_along_path(distance):
+	var last_point = self.position
+	while path.size():
+		var distance_between_points = last_point.distance_to(path[0])
+		if distance <= distance_between_points:
+			self.position = last_point.lerp(path[0], distance / distance_between_points)
+			return
+		
+		distance -= distance_between_points
+		last_point = path[0]
+		path.remove_at(0)
+	self.position = last_point
+	set_process(false)
+
 func recalc_path():
 	if target_node:
 		nav_agent.target_position = target_node.global_position
 	else:
 		nav_agent.target_position = home_pos
 		
+func setup_navserver ():
+	#create a new navigation map
+	map = NavigationServer2D.map_create()
+	NavigationServer2D.map_set_active(map, true)
+	
+	#create a new navigation region and add it to the map
+	var region = NavigationServer2D.region_create()
+	NavigationServer2D.region_set_transform(region, Transform2D())
+	NavigationServer2D.region_set_map(region, map)
+	
+	#set navigation mesh for the region
+	var navigation_poly = NavigationMesh.new()
+	navigation_poly = navReg.navigation_polygon
+	NavigationServer2D.region_set_navigation_polygon(region, navigation_poly)
 
-		
-
-
+func _update_navigation_path(start_position, end_position):
+	path = NavigationServer2D.map_get_path(map, start_position, end_position, true)
+	path.remove_at(0)
+	set_process(true)
+	
 func _on_nav_update_timer_timeout() -> void:
-	recalc_path()
-
+	#recalc_path()
+	if target:
+		target_node = target.global_position
+		_update_navigation_path(self.position, target_node)
+	else:
+		_update_navigation_path(self.position, home_pos)
 
 func _on_aggro_range_area_entered(area: Area2D) -> void:
-	target_node = area.owner
-
+	#target_node = area.owner.global_position
+	target = area.owner
+	print("player detected")
+	
 
 func _on_de_aggro_range_area_exited(area: Area2D) -> void:
-	if area.owner == target_node:
-		target_node = null
+	if area.owner == target:
+		#target_node = null
+		target = null
+		
+		print("player lost")
